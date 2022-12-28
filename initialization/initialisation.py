1a2f6b897781856a5e239a0b14484ce72a1857d5
keyshavmor <keyshavmor2611@gmail.com>
2022-12-28 22:11:35 +0100

    REFACT: Adde description to initialisation.py

--- initialization/initialisation.py
+++ initialization/initialisation.py
@@ -1,1 +1,15 @@
 # Initialisation file in Python to obtain 2 initial frames, bootstrap their poses
+
+# The initialization file takes an input of two images (keyframes) which are then used to
+# obtain keypoints on image plane and corresponding 3D keypoints in world co-ordinate system. 
+# The transformation matrix to convert image points to 3D world coordinate system is also computed.
+#
+#   INPUT:
+#       img_0                 : first image of data set
+#       img_n                 : next image (nth image of dataset separated by a sufficient baseline)
+#       camera_intrinsics     : Camera calibration matrix K and initial pose matrix P.
+#
+#   OUTPUT:
+#       transformation_matrix : Dimension - 4x4, transform from image frame to 3D world co-ordination system
+#       initial_keypoints     : Dimension - Nx2, N is number of keypoints. Initial keypoints in the image plane                   
+#       initial_landmarks     : Dimension - Nx2, N is number of landmarks. initial landmarks which where triangulated from intial keypoints.
