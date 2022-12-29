cd9cfb537acaa17674c8279c3b2564a032a41403
keyshavmor <keyshavmor2611@gmail.com>
2022-12-29 22:18:16 +0100

    REFACT: Added initial main code skeleton

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+import dataset_params
+
 # We implement the visual odometry pipeline initially only for 3 datasets
 
 num_of_datasets = 3
 dataset_dictionary = {"parking":1, "KITTI":2, "Malaga":3}
 
+user_dataset = input("Pick User Dataset (parking, KITTI, Malaga): ")
+
+current_dataset = user_dataset
+
+if(current_dataset == "parking"):
+
+    params = dataset_params.parking_dataset_parameters()
+    print(params)
+else:
+    print("Program not yet defined for this dataset")
