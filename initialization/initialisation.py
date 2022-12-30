91cf5c2efc1f16e8d413e8704f4c4dffbdf1cc2e
keyshavmor <keyshavmor2611@gmail.com>
2022-12-31 00:45:17 +0100

    REFACT: Added keypoint detection and descriptor extraction code

--- initialization/initialisation.py
+++ initialization/initialisation.py
@@ -27,6 +27,104 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+def detect_keypoints_descriptors(image_0, image_1):
+
+    corner_points_0 = cv.cornerHarris(image_0, 2, 3, 0.04)
+    threshold_factor_0 = (0.0000001)/(corner_points_0.max())
+    # Threshold the corner points
+    corner_points_0 = corner_points_0 > threshold_factor_0 * corner_points_0.max()
+
+    # Convert the corner points to coordinates
+    coords = np.column_stack(np.nonzero(corner_points_0))
+    image = cv.cvtColor(image_0, cv.COLOR_GRAY2RGB)
+    # Draw the corner points on the image
+    for coord in coords:
+        x, y = coord
+        image = cv.circle(image, (y, x), 1, (0, 0, 255), 1) # Co-ordinates need to be inverted to fit the image
+    # Display the image
+    cv.imshow('image', image)
+    cv.waitKey(1000)
+    cv.destroyWindow('image')
+
+    corner_points_1 = cv.cornerHarris(image_1, 2, 3, 0.04)
+    threshold_factor_1 = (0.0000001)/(corner_points_1.max())
+    # Threshold the corner points
+    corner_points_1 = corner_points_1 > threshold_factor_1 * corner_points_1.max()
+    # Convert the corner points to coordinates
+    coords = np.column_stack(np.nonzero(corner_points_1))
+    image = cv.cvtColor(image_1, cv.COLOR_GRAY2RGB)
+    # Draw the corner points on the image
+    for coord in coords:
+        x, y = coord
+        image = cv.circle(image, (y, x), 1, (0, 0, 255), 1) # Co-ordinates need to be inverted to fit the image
+    # Display the image
+    cv.imshow('image', image)
+    cv.waitKey(1000)
+    cv.destroyWindow('image')
+
+    # Create a list of keypoints from the corner points
+    keypoints_0 = []
+    for y in range(corner_points_0.shape[0]):
+        for x in range(corner_points_0.shape[1]):
+            if corner_points_0[y, x] > 0:
+                keypoint = cv.KeyPoint(x, y, size=10)
+                keypoints_0.append(keypoint)
+
+    # Create a SIFT descriptor object
+    sift = cv.SIFT_create()
+
+    # Compute feature vectors (descriptors) for the keypoints
+    valid_kp_0, descriptor_0 = sift.compute(image_0, keypoints_0)
+
+    # Create a list of keypoints from the corner points
+    keypoints_1 = []
+    for y in range(corner_points_1.shape[0]):
+        for x in range(corner_points_1.shape[1]):
+            if corner_points_1[y, x] > 0:
+                keypoint = cv.KeyPoint(x, y, size=10)
+                keypoints_1.append(keypoint)
+
+    # Create a SIFT descriptor object
+    sift = cv.SIFT_create()
+
+    # Compute feature vectors (descriptors) for the keypoints
+    valid_kp_1, descriptor_1 = sift.compute(image_1, keypoints_1)
+
+    # Draw the keypoints and their descriptors on the image
+    img0_sift = cv.drawKeypoints(image_0, valid_kp_0, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Display the image
+    cv.imshow("SIFT Descriptors", img0_sift)
+    cv.waitKey(1000)
+    cv.destroyWindow("SIFT Descriptors")
+
+    # Draw the keypoints and their descriptors on the image
+    img1_sift = cv.drawKeypoints(image_1, valid_kp_1, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Display the image
+    cv.imshow("SIFT Descriptors", img1_sift)
+    cv.waitKey(1000)
+    cv.destroyWindow("SIFT Descriptors")
+
+    return valid_kp_0, valid_kp_1, img0_sift, img1_sift, descriptor_0, descriptor_1
+
+#def descriptor_keypoints(image_0, image_1, keypoints_0, keypoint_1):
+    
+    # Create a SIFT descriptor object
+#    sift_0 = cv.SIFT_create()
+
+    # Compute feature vectors (descriptors) for the corner points
+#    valid_kp_0, descriptor_0 = sift_0.compute(image_0, keypoints_0)
+
+    # Create a SIFT descriptor object
+#    sift_1 = cv.SIFT_create()
+
+    # Compute feature vectors (descriptors) for the corner points
+#    valid_kp_1, descriptor_1 = sift_1.compute(image_1, keypoints_1)
+
+#    return descriptor_0, valid_kp_0, descriptor_1, valid_kp_1
+
+
 # Detect keypoints in the two images
 def init(dataset_params):
 
@@ -42,7 +140,7 @@ def init(dataset_params):
     
         # Obtain Keyframe 0
         image = cv.imread(path)
-        image_0 = image.astype(np.uint8)
+        image_0 = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
         cv.imshow("Image", image_0)
         cv.waitKey(1000)
         cv.destroyWindow("Image")
@@ -57,40 +155,37 @@ def init(dataset_params):
 
         # Obtain Keyframe 1
         image = cv.imread(path)
-        image_1 = image.astype(np.uint8)
+        image_1 = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
         cv.imshow("Image", image_1)
         cv.waitKey(1000)
         cv.destroyWindow("Image")
 
     # Detect keypoints
-    keypoints_0, keypoint_1 = detect_keypoints(image_0, image_1, dataset_params)
-
-    # Descriptor keypoints
-    descriptor_0, valid_kp_0, descriptor_1, valid_kp_1 = descriptor_keypoints(image_0, image_1, keypoints_0, keypoint_1, dataset_params)
+    keypoints_0, keypoint_1, image_0, image_1, descriptor_0, descriptor_1 = detect_keypoints_descriptors(image_0, image_1)
 
     # Match Keypoints
-    matched_kp_0, matched_kp_1 = match_keypoints(image_0, image_1, valid_kp_0, valid_kp_1, descriptor_0, descriptor_1, params)
+    #matched_kp_0, matched_kp_1 = match_keypoints(image_0, image_1, valid_kp_0, valid_kp_1, descriptor_0, descriptor_1, params)
 
     # Compute relative pose of second image
-    orientation, localization, inliers = relative_pose(image_0, image_1, matched_kp_0, matched_kp_1, dataset_params)
+    #orientation, localization, inliers = relative_pose(image_0, image_1, matched_kp_0, matched_kp_1, dataset_params)
 
     # Triangulate landmarks from matched keypoints
-    transformation_matrix, initial_landmarks = triangulate(matched_kp_0, matched_kp_1, inliers, orientation, localization, dataset_params)
+    #transformation_matrix, initial_landmarks = triangulate(matched_kp_0, matched_kp_1, inliers, orientation, localization, dataset_params)
 
     #Visualise matched features and inliers
 
     # Use cv2.drawMatches to display the matched keypoints
-    matched_features = cv2.drawMatches(image_0, matched_kp_0, image_1, matched_kp_1, None)
+    #matched_features = cv2.drawMatches(image_0, matched_kp_0, image_1, matched_kp_1, None)
 
     # Display the output image
-    cv2.imshow("Matched keypoints", matched_features)
-    cv2.waitKey(1000)
-    cv2.destroyWindow("Matched keypoints")
+    #cv2.imshow("Matched keypoints", matched_features)
+    #cv2.waitKey(1000)
+    #cv2.destroyWindow("Matched keypoints")
 
     # Use cv2.drawMatches to display the inlier matched keypoints
-    matched_inliers = cv2.drawMatches(image_0, matched_kp_0, image_1, matched_kp_1, inliers, None)
+    #matched_inliers = cv2.drawMatches(image_0, matched_kp_0, image_1, matched_kp_1, inliers, None)
 
     # Display the output image
-    cv2.imshow("Matched inliers", matched_inliers)
-    cv2.waitKey(1000)
-    cv2.destroyWindow("Matched inliers")
+    #cv2.imshow("Matched inliers", matched_inliers)
+    #cv2.waitKey(1000)
+    #cv2.destroyWindow("Matched inliers")
